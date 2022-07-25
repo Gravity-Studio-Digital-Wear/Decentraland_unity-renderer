f4733a12578718b4cca72659e03cf7e2a70aa98c
Semih Guresci <semih@gravitylayer.io>
2022-07-25 15:20:21 +0100

    light saber jacket

--- unity-renderer/Assets/UnityGLTF/Scripts/Loader/WebRequestLoader.cs
+++ unity-renderer/Assets/UnityGLTF/Scripts/Loader/WebRequestLoader.cs
@@ -78,7 +78,8 @@ namespace UnityGLTF.Loader
                 // Male Black Jacket
                 if (String.Equals("https://peer.decentral.io/content/contents/QmX6NmvbLJv2CiXAQy2ynXMHEhy8bR5suJNr3gXYcMBRpg", finalUrl, StringComparison.InvariantCultureIgnoreCase))
                 {
-                    finalUrl = "https://wearables-storage.nyc3.digitaloceanspaces.com/wearables/Polygon_Mumbai/0x2953399124f0cbb46d2cbacd8a89cf0599974963_75437324160650951662245703982020702172073797313123328702383515790577235918948/Decentraland/jacket_Decentraland_armstest_newmat.glb";
+                    // finalUrl = "https://wearables-storage.nyc3.digitaloceanspaces.com/wearables/Polygon_Mumbai/0x2953399124f0cbb46d2cbacd8a89cf0599974963_75437324160650951662245703982020702172073797313123328702383515790577235918948/Decentraland/jacket_Decentraland_armstest_newmat.glb";
+                    finalUrl = "https://peer.decentral.io/content/contents/QmXA12se62vPFWU9GusXaYP6iM4XTUECHTUkrzFMRUzx9a";
                 }
                 // Female Red Jacket
                 else if (String.Equals("https://peer.decentral.io/content/contents/QmYcSYPMsT4678MhepUPsqtHGDLQArjEETmKA1i1PCBCWM", finalUrl, StringComparison.InvariantCultureIgnoreCase))
